9a90742bd59fb4379e4578a4bb964fcd39e9a5c7
VilmaIkonen <vilma.r.ikonen@gmail.com>
2020-10-26 16:48:47 +0200

    2_task added

---
+++ 2_task/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function Content() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Content />
+    </div>
+  );
+}
+
+export default App;
