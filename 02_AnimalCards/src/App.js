7837d6c5fc0ba85760589e61dceb825bd34a2e3d
VilmaIkonen <vilma.r.ikonen@gmail.com>
2020-10-29 13:31:35 +0200

    02_AnimalCards changed to class components

--- 02_AnimalCards/src/App.js
+++ 02_AnimalCards/src/App.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Animal from './Components/Animal/Animal';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="cards">
-      <Animal
-        name="Snowly Owl"
-        image="https://source.unsplash.com/DvZpQRpirlo"
-      />
-      <Animal name="Meerkat" image="https://source.unsplash.com/FQjmQgSoRyQ" />
-      <Animal
-        name="Field Mouse"
-        image="https://source.unsplash.com/ocFm38bEyTE"
-      />
-    </div>
-  );
-};
+// Adding of class component. class comp. can do rendering inside the component, do not need to render whole app.
+// State always as the first thing
+class App extends Component {
+  state = {
+    animals: [
+      { name: 'Snowy Owl', image: 'https://source.unsplash.com/DvZpQRpirlo' },
+      { name: 'Meerkat', image: 'https://source.unsplash.com/FQjmQgSoRyQ' },
+      { name: 'Field Mouse', image: 'https://source.unsplash.com/ocFm38bEyTE' },
+    ],
+  };
+  render() {
+    return (
+      <div className="cards">
+        <Animal
+          name={this.state.animals[0].name}
+          image={this.state.animals[0]}
+        />
+        <Animal
+          name={this.state.animals[1].name}
+          image={this.state.animals[1].image}
+        />{' '}
+        <Animal
+          name={this.state.animals[2].name}
+          image={this.state.animals[2].image}
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
