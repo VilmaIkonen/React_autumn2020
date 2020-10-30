bb8268c4163e30e29b90b73ac0f2e48f92ec3445
VilmaIkonen <vilma.r.ikonen@gmail.com>
2020-10-30 14:22:31 +0200

    Skeleton for task 4

---
+++ 04_task/src/Components/Article.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Article = () => {
+  return (
+    <div>
+      <img
+        alt="colourful laptop on dark bakground"
+        src="https://source.unsplash.com/u76CN5rZeOU"
+      ></img>
+      <h2>Full Stack Web Developer program</h2>
+      <p>
+        Full Stack Web Developer program is designd for adults who want to works
+        as software developers. Studying in the program is very practical in
+        comparison to academic studies. The studies focus 100% on getting the
+        core skills needed as software developer.
+      </p>
+    </div>
+  );
+};
+
+export default Article;
---
+++ 04_task/src/Components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Article from './Article';
+
+const Main = () => {
+  return (
+    <div>
+      <div className="container">
+        <Article />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
