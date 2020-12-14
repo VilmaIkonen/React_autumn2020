772fd863ad61c04d4619f7ec81d1069f3fa78817
VilmaIkonen <vilma.r.ikonen@gmail.com>
2020-12-14 12:31:23 +0200

    Adding React Bootstrap dependencies

--- 09_task_nested_routing/src/App.js
+++ 09_task_nested_routing/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom'
 
--- 09_task_nested_routing/src/Components/Header/Nav.js
+++ 09_task_nested_routing/src/Components/Header/Nav.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Navbar } from 'react-bootstrap';
 
 /* Don't do navigation like that, use Router instead */
 const Nav = () => {
 	return (
-		<nav> 
-			<ul>
-				<li> <Link to="/">Home</Link> </li>
-				<li> <Link to="/blog">Blog</Link> </li>
-				<li> <Link to="/new_post">New Post</Link> </li>
-			</ul>
-		</nav>
-	);
+		// <nav> 
+		// 	<ul>
+		// 		<li> <Link to="/">Home</Link> </li>
+		// 		<li> <Link to="/blog">Blog</Link> </li>
+		// 		<li> <Link to="/new_post">New Post</Link> </li>
+		// 	</ul>
+		// </nav>
+	<div>Hello!</div>);
 };
 
 export default Nav;
